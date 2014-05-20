ccc7ffee00736fb87edda1cc918b93d29d5cb423
Ilkka Seppala <iluwatar@gmail.com>
2014-05-20 23:26:27 +0300

    sample takes foundation resource references from foundation-core

--- foundation-samples/src/main/java/com/iluwatar/HomePage.java
+++ foundation-samples/src/main/java/com/iluwatar/HomePage.java
@@ -1,13 +1,26 @@
 package com.iluwatar;
 
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import com.iluwatar.foundation.Foundation;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
-
     }
+
+	@Override
+	public void renderHead(IHeaderResponse response) {
+		super.renderHead(response);
+		response.render(CssHeaderItem.forReference(Foundation.getFoundationCssReference()));
+		response.render(JavaScriptHeaderItem.forReference(Foundation.getFoundationJsReference()));
+	}
+	
+	
 }
