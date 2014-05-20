94082f8eb9c611b9d0833993bc3783992a8c0b3c
Ilkka Seppala <iluwatar@gmail.com>
2014-05-20 23:09:00 +0300

    added Foundation.java with css and javascript resource references

---
+++ foundation-core/src/main/java/com/iluwatar/Foundation.java
@@ -0,0 +1,27 @@
+package com.iluwatar;
+
+import org.apache.wicket.request.resource.CssResourceReference;
+import org.apache.wicket.request.resource.JavaScriptResourceReference;
+import org.apache.wicket.request.resource.ResourceReference;
+
+public class Foundation {
+
+	private static ResourceReference foundationCssReference;
+	
+	private static ResourceReference foundationJsReference;
+	
+	public static ResourceReference getFoundationCssReference() {
+		if (foundationCssReference == null) {
+			foundationCssReference = new CssResourceReference(Foundation.class, "foundation.css");
+		}
+		return foundationCssReference;
+	}
+
+	public static ResourceReference getFoundationJsReference() {
+		if (foundationJsReference == null) {
+			foundationJsReference = new JavaScriptResourceReference(Foundation.class, "foundation.js");
+		}
+		return foundationJsReference;
+	}
+	
+}
