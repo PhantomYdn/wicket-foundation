caf1faee58366d35de1a833a5b84211939737d0c
Ilkka Seppala <iluwatar@gmail.com>
2014-05-29 22:14:48 +0300

    added block grid sample

---
+++ foundation-samples/src/main/java/com/iluwatar/BlockGridPage.java
@@ -0,0 +1,13 @@
+package com.iluwatar;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class BlockGridPage extends BasePage {
+
+	private static final long serialVersionUID = 1L;
+
+	public BlockGridPage(PageParameters params) {
+		super(params);
+	}
+
+}
