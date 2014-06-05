5b0f48ee0dae1c7ea8c6a1250a2f6df912d7627b
Ilkka Seppala <iluwatar@gmail.com>
2014-06-05 23:47:13 +0300

    work on catalog page and visibility sample

---
+++ foundation-core/src/main/java/com/iluwatar/foundation/behavior/ShowByScreenSizeBehavior.java
@@ -0,0 +1,25 @@
+package com.iluwatar.foundation.behavior;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.behavior.Behavior;
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.head.IHeaderResponse;
+
+public class ShowByScreenSizeBehavior extends Behavior {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void onComponentTag(Component component, ComponentTag tag) {
+		// TODO Auto-generated method stub
+		super.onComponentTag(component, tag);
+	}
+
+	@Override
+	public void renderHead(Component component, IHeaderResponse response) {
+		// TODO Auto-generated method stub
+		super.renderHead(component, response);
+	}
+
+	
+}
