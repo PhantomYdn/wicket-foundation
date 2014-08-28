69508807d3d773f1df6f55dabab7933c38761ede
Ilkka Seppala <iluwatar@gmail.com>
2014-08-28 23:15:34 +0300

    Added buttons sample page.

---
+++ foundation-samples/src/main/java/com/iluwatar/ButtonsPage.java
@@ -0,0 +1,13 @@
+package com.iluwatar;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class ButtonsPage extends BasePage {
+
+	private static final long serialVersionUID = 1L;
+
+	public ButtonsPage(PageParameters params) {
+		super(params);
+	}
+
+}
