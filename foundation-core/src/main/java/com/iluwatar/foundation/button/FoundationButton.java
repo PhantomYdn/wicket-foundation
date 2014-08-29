9df61444444c0f336ef3b379c3c64468907b313c
Ilkka Seppala <iluwatar@gmail.com>
2014-08-29 23:37:30 +0300

    Added FoundationButtonBehavior. Modified FoundationButton to use it.

--- foundation-core/src/main/java/com/iluwatar/foundation/button/FoundationButton.java
+++ foundation-core/src/main/java/com/iluwatar/foundation/button/FoundationButton.java
@@ -2,16 +2,10 @@ package com.iluwatar.foundation.button;
 
 import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.MarkupStream;
-import org.apache.wicket.markup.head.CssHeaderItem;
-import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.util.lang.Args;
-
-import com.iluwatar.foundation.Foundation;
-import com.iluwatar.foundation.util.Attribute;
-import com.iluwatar.foundation.util.StringUtil;
 
 public class FoundationButton extends Button {
 
@@ -22,63 +16,31 @@ public class FoundationButton extends Button {
 	private IModel<FoundationButtonOptions> optionsModel;
 
 	public FoundationButton(String id) {
-		super(id);
-		optionsModel = Model.of(new FoundationButtonOptions());
+		this(id, new FoundationButtonOptions());
 	}
 
 	public FoundationButton(String id, FoundationButtonOptions options) {
 		super(id);
+		Args.notNull(options, "options");
 		optionsModel = Model.of(options);
+		add(new FoundationButtonBehavior(optionsModel));
 	}
 	
 	public FoundationButton(String id, IModel<String> model) {
 		super(id, model);
 		optionsModel = Model.of(new FoundationButtonOptions());
+		add(new FoundationButtonBehavior(optionsModel));
 	}
 
 	public FoundationButton(String id, IModel<String> model, FoundationButtonOptions options) {
-		super(id, model);
-		Args.notNull(options, "options");
-		this.optionsModel = Model.of(options);
+		this(id, model, Model.of(options));
 	}
 	
 	public FoundationButton(String id, IModel<String> model, IModel<FoundationButtonOptions> optionsModel) {
 		super(id, model);
 		Args.notNull(optionsModel, "optionsModel");
 		this.optionsModel = optionsModel;
-	}
-	
-	@Override
-	public void renderHead(IHeaderResponse response) {
-		super.renderHead(response);
-		response.render(CssHeaderItem.forReference(Foundation.getFoundationCssReference()));
-	}
-	
-	@Override
-	protected void onComponentTag(ComponentTag tag) {
-		Attribute.setClass(tag, "button");
-		FoundationButtonOptions options = optionsModel.getObject();
-		if (options.getFoundationButtonSize() != null) {
-			Attribute.addClass(tag, StringUtil.EnumNameToCssClassName(options
-					.getFoundationButtonSize().name()));
-		}
-		if (options.getFoundationButtonColor() != null) {
-			Attribute.addClass(tag, StringUtil.EnumNameToCssClassName(options
-					.getFoundationButtonColor().name()));
-		}
-		if (options.getFoundationButtonRadius() != null) {
-			Attribute.addClass(tag, StringUtil.EnumNameToCssClassName(options
-					.getFoundationButtonRadius().name()));
-		}
-		if (options.getFoundationButtonState() != null) {
-			Attribute.addClass(tag, StringUtil.EnumNameToCssClassName(options
-					.getFoundationButtonState().name()));
-		}
-		if (options.getFoundationButtonExpansion() != null) {
-			Attribute.addClass(tag, StringUtil.EnumNameToCssClassName(options
-					.getFoundationButtonExpansion().name()));
-		}
-		super.onComponentTag(tag);
+		add(new FoundationButtonBehavior(optionsModel));
 	}
 	
 	@Override
