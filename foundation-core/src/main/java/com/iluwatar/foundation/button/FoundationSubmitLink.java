5ce081c933ad186cf13fe23bdbbf8aaf63e77e86
Ilkka Seppala <iluwatar@gmail.com>
2014-08-30 15:40:30 +0300

    Added FoundationSubmitLink and tests.

---
+++ foundation-core/src/main/java/com/iluwatar/foundation/button/FoundationSubmitLink.java
@@ -0,0 +1,66 @@
+package com.iluwatar.foundation.button;
+
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.MarkupStream;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.SubmitLink;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.util.lang.Args;
+
+public class FoundationSubmitLink extends SubmitLink {
+
+	private static final long serialVersionUID = 1L;
+
+	private IModel<FoundationButtonOptions> optionsModel;
+
+	public FoundationSubmitLink(String id) {
+		this(id, new FoundationButtonOptions());
+	}
+	
+	public FoundationSubmitLink(String id, FoundationButtonOptions options) {
+		super(id);
+		Args.notNull(options, "options");
+		optionsModel = Model.of(options);
+		add(new FoundationButtonBehavior(optionsModel));
+	}
+	
+	public FoundationSubmitLink(String id, Form<?> form, FoundationButtonOptions options) {
+		super(id, form);
+		Args.notNull(options, "options");
+		optionsModel = Model.of(options);
+		add(new FoundationButtonBehavior(optionsModel));
+	}
+	
+	public FoundationSubmitLink(String id, IModel<?> model, FoundationButtonOptions options) {
+		super(id, model);
+		Args.notNull(options, "options");
+		optionsModel = Model.of(options);
+		add(new FoundationButtonBehavior(optionsModel));
+	}
+	
+	public FoundationSubmitLink(String id, IModel<?> model, Form<?> form, FoundationButtonOptions options) {
+		this(id, model, form, Model.of(options));
+	}
+
+	public FoundationSubmitLink(String id, IModel<?> model, Form<?> form, IModel<FoundationButtonOptions> optionsModel) {
+		super(id, model, form);
+		Args.notNull(optionsModel, "optionsModel");
+		this.optionsModel = optionsModel;
+		add(new FoundationButtonBehavior(optionsModel));
+	}
+	
+	@Override
+	public void onComponentTagBody(MarkupStream markupStream,
+			ComponentTag openTag) {
+		this.replaceComponentTagBody(markupStream, openTag, this.getDefaultModelObjectAsString());
+		super.onComponentTagBody(markupStream, openTag);
+	}
+	
+	@Override
+	protected void onDetach() {
+		this.optionsModel.detach();
+		super.onDetach();
+	}
+	
+}
