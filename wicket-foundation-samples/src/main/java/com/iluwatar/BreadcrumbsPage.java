b3c5a195123543d35d663defc51fd42d0ca9021e
Ilkka Seppala <iluwatar@gmail.com>
2014-08-31 22:24:27 +0300

    Started work on breadcrumbs.

---
+++ wicket-foundation-samples/src/main/java/com/iluwatar/BreadcrumbsPage.java
@@ -0,0 +1,13 @@
+package com.iluwatar;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class BreadcrumbsPage extends BasePage {
+
+	private static final long serialVersionUID = 1L;
+
+	public BreadcrumbsPage(PageParameters params) {
+		super(params);
+	}
+
+}
