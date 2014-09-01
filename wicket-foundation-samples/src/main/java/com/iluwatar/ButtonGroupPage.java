e286cec85645771f33952b6d0ec04d020d58fbe6
Ilkka Seppala <iluwatar@gmail.com>
2014-09-01 16:53:15 +0300

    Added button groups sample page and linked from catalog.

---
+++ wicket-foundation-samples/src/main/java/com/iluwatar/ButtonGroupPage.java
@@ -0,0 +1,13 @@
+package com.iluwatar;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class ButtonGroupPage extends BasePage {
+
+	private static final long serialVersionUID = 1L;
+
+	public ButtonGroupPage(PageParameters params) {
+		super(params);
+	}
+
+}
