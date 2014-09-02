ce5add33739c168591bf40d9534298009eff4850
Ilkka Seppala <iluwatar@gmail.com>
2014-09-02 11:39:43 +0300

    Updated button group design.

--- wicket-foundation-core/src/main/java/com/iluwatar/foundation/buttongroup/ButtonGroupPanel.java
+++ wicket-foundation-core/src/main/java/com/iluwatar/foundation/buttongroup/ButtonGroupPanel.java
@@ -1,11 +1,16 @@
 package com.iluwatar.foundation.buttongroup;
 
+import java.util.List;
+
 import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.WebMarkupContainer;
-import org.apache.wicket.markup.repeater.RepeatingView;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.model.util.ListModel;
 
+import com.iluwatar.foundation.button.FoundationButtonOptions;
 import com.iluwatar.foundation.component.FoundationBasePanel;
 import com.iluwatar.foundation.util.Attribute;
 
@@ -13,28 +18,36 @@ public abstract class ButtonGroupPanel extends FoundationBasePanel {
 
 	private static final long serialVersionUID = 1L;
 
-	public ButtonGroupPanel(String id) {
-		this(id, new ButtonGroupOptions());
+	public ButtonGroupPanel(String id, ButtonGroupOptions groupOptions, List<FoundationButtonOptions> btnOptions) {
+		this(id, Model.of(groupOptions), new ListModel<FoundationButtonOptions>(btnOptions));
 	}
 	
-	public ButtonGroupPanel(String id, ButtonGroupOptions groupOptions) {
-		this(id, Model.of(groupOptions));
-	}
-	
-	public ButtonGroupPanel(String id, IModel<ButtonGroupOptions> groupOptions) {
+	public ButtonGroupPanel(String id, IModel<ButtonGroupOptions> groupOptionsModel, IModel<List<FoundationButtonOptions>> btnOptionsModel) {
 		super(id);
-		WebMarkupContainer container = createContainer("group", groupOptions);
+		WebMarkupContainer container = createContainer("group", groupOptionsModel);
 		add(container);
-		RepeatingView rv = new RepeatingView("btn");
-		container.add(rv);
-		createAndAddButtons(rv);
+		ListView<FoundationButtonOptions> lv = createRepeater("item", btnOptionsModel);
+		container.add(lv);
 	}
 	
 	protected WebMarkupContainer createContainer(String id, IModel<ButtonGroupOptions> groupOptions) {
 		return new ButtonGroupContainer(id, groupOptions);
 	}
 	
-	protected abstract void createAndAddButtons(RepeatingView rv);
+	protected ListView<FoundationButtonOptions> createRepeater(String id, IModel<List<FoundationButtonOptions>> btnOptionsModel) {
+		return new ListView<FoundationButtonOptions>(id, btnOptionsModel) {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected void populateItem(ListItem<FoundationButtonOptions> item) {
+				WebMarkupContainer btn = ButtonGroupPanel.this.createButton(item.getIndex(), "btn", item.getModel());
+				item.add(btn);
+			}
+		};
+	}
+	
+	protected abstract WebMarkupContainer createButton(int idx, String id, IModel<FoundationButtonOptions> optionsModel);
 
 	private static class ButtonGroupContainer extends WebMarkupContainer {
 
--- wicket-foundation-core/src/test/java/com/iluwatar/foundation/buttongroup/ButtonGroupPanelTest.java
+++ wicket-foundation-core/src/test/java/com/iluwatar/foundation/buttongroup/ButtonGroupPanelTest.java
@@ -1,33 +1,45 @@
 package com.iluwatar.foundation.buttongroup;
 
-import org.apache.wicket.markup.repeater.RepeatingView;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Test;
 
 import com.iluwatar.foundation.button.FoundationButton;
+import com.iluwatar.foundation.button.FoundationButtonOptions;
 
 public class ButtonGroupPanelTest {
 
 	@Test
 	public void renderTest() { 
 		WicketTester tester = new WicketTester();
-		TestButtonGroupPanel group = new TestButtonGroupPanel("buttons");
-		tester.startComponentInPage(group);
+		tester.startComponentInPage(createButtonGroup());
 		tester.dumpPage();
+	}
+	
+	private TestButtonGroupPanel createButtonGroup() {
+		ArrayList<FoundationButtonOptions> btnOptions = new ArrayList<>();
+		btnOptions.add(new FoundationButtonOptions());
+		ButtonGroupOptions groupOptions = new ButtonGroupOptions();
+		TestButtonGroupPanel group = new TestButtonGroupPanel("buttons", groupOptions, btnOptions);
+		return group;
 	}
 	
 	private static class TestButtonGroupPanel extends ButtonGroupPanel {
 
-		public TestButtonGroupPanel(String id) {
-			super(id);
-		}
-
 		private static final long serialVersionUID = 1L;
 		
-		@Override
-		protected void createAndAddButtons(RepeatingView rv) {
-			rv.add(new FoundationButton(rv.newChildId()));
+		public TestButtonGroupPanel(String id, ButtonGroupOptions groupOptions,
+				List<FoundationButtonOptions> btnOptions) {
+			super(id, groupOptions, btnOptions);
 		}
-		
+
+		@Override
+		protected WebMarkupContainer createButton(int idx, String id, IModel<FoundationButtonOptions> optionsModel) {
+			return new FoundationButton(id, optionsModel.getObject());
+		}
 	}
 }
