ef15d24569e9c4aae7e0cd2adcd3b3795172666f
Ilkka Seppala <iluwatar@gmail.com>
2014-09-03 21:26:56 +0300

    Added alerts sample page and rendering test.

---
+++ wicket-foundation-samples/src/main/java/com/iluwatar/AlertsPage.java
@@ -0,0 +1,12 @@
+package com.iluwatar;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class AlertsPage extends BasePage {
+
+	private static final long serialVersionUID = 1L;
+
+	public AlertsPage(PageParameters params) {
+		super(params);
+	}
+}
